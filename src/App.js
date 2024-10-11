fb438793a1341cec45c8db033b9b7aa0e9238222
MDjafar07 <mdjafarshk786@gmail.com>
2024-10-11 16:34:51 +0530

    todo_list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React,{useState,useEffect,useRef,useCallback} from 'react'
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
-function App() {
+const App=() =>{
+
+  const [todos,setTodos]= useState([]);
+  const [userInput,setUseInput]=useState('');
+  const ref=useRef(null);
+
+  const addTodoHandler=useCallback(()=>{
+      const oldTodos=[...todos];
+      if(userInput ==="")
+      {
+        return;
+      }
+      else{
+          const newTodo={
+            id:Math.floor(Math.random()*100),
+            text:userInput
+          };
+          const newTodos=oldTodos.concat(newTodo);
+          setTodos(newTodos);
+      }
+      setUseInput("")
+
+  },[todos,userInput])
+
+  useEffect(()=>{
+    ref.current.focus()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+       <div className='container'>
+        <span>Total Todolist : {todos.length}</span>
+        <div className='input-container'>
+          <input type='text' value={userInput} onChange={(event)=>setUseInput(event.target.value)}
+            ref={ref}
+          />
+          <button onClick={addTodoHandler}>Add</button>
+        </div>
+        <div className='todos-container-parent'>
+          {todos.map((todo,index)=>(
+              <div key={todo.id}>{todo.text}</div>
+          ))}
+        </div>
+       </div>
+      <Footer />
     </div>
   );
 }
